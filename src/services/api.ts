b23fb36f022d2e464022268165303bff80d16529
Satyendra Kumar <77930557+Satyendra13@users.noreply.github.com>
2025-05-05 02:47:51 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.API_URL;
 
 export const scrapeWebsite = async (url: string) => {
   return axios.post(`${API_URL}/websites/scrape`, { url });
